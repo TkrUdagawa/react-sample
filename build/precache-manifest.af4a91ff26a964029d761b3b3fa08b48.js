701484d37e7345ab9c69a8a442df295ce97dca6a
TkrUdagawa <tkr.udagawa.lab@gmail.com>
2020-06-10 21:58:30 +0900

    add build

---
+++ build/precache-manifest.af4a91ff26a964029d761b3b3fa08b48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb73a07ca0a5e9e6c23b1cb6436f902",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4478d0cd7fb27c5debcd",
+    "url": "/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "885d8d2e189bcdc5736d",
+    "url": "/static/js/2.2ea94aec.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.2ea94aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4478d0cd7fb27c5debcd",
+    "url": "/static/js/main.9d51e107.chunk.js"
+  },
+  {
+    "revision": "9e2aa31c9bfd85c0548b",
+    "url": "/static/js/runtime-main.ce7fd491.js"
+  }
+]);
